163c5ffd8cf566a540592c059c1f711799e55e88
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-04-28 20:49:30 +0000

    commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 def fizzbuzz():
-    for num in range(1, 101):
+    for num in range(1, 100 + 1):
         if num % 3 == 0:
             print("Fizz", end='')
         if num % 5 == 0:
             print("Buzz", end='')
         if num % 3 and num % 5:
             print("{:d}".format(num), end='')
-print(end=' ')
+        print(end=' ')
