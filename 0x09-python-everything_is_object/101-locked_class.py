594bbfc335670e09f255da6964b3d88450a3f2ee
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-05-26 22:17:16 +0100

    commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+   """
+   A Locked class
+   """
 class LockedClass:
-    """A class that prevents the user from dynamically creating
-    new instance attributes"""
+    """
+    A class that prevents the user from dynamically creating
+    new instance attributes
+    """
     __slots__ = ['first_name']
