30f36e8713d8d24928cd784d4617595c11708800
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-06-03 12:39:17 +0100

    commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class base geometry module """
+
+
+class BaseGeometry(object):
+    """ empty base geometry class """
+    def area(self):
+        """exception with a message"""
+        raise Exception("area() is not implemented")
