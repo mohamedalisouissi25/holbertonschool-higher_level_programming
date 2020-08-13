cd30b49c44595ec81e4fa8d7e4cc0fe8f0f8f58c
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 13:49:06 +0100

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-
-""" a script that lists all states from the database hbtn_0e_0_usa with
-a given name """
+""" displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument
+"""
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
 
-    dal = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                          passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
+    dal = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = dal.cursor()
+    que = "SELECT * FROM states\
+    WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4])
+    cursor.execute(query)
     rows = cursor.fetchall()
-
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-    cur.close()
+        print(row)
+    cursor.close()
     dal.close()
+
