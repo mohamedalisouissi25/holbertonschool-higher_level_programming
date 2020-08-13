4bc73480cd85a702a626d379a01bb0edad962de5
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 18:08:28 +0100

    commit

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     dal = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = dal.cursor()
-    cursor.execute("SELECT * FROM states\ WHERE name LIKE %s ORDER BY id ASC",
-                   (argv[4],))
+    cursor.execute("SELECT * FROM states\
+                   WHERE name LIKE %s ORDER BY id ASC", (sys.argv[4],))
     rows = cursor.fetchall()
 
     for row in rows:
