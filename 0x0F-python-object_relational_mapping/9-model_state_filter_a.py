50fc7e8dc2a21c0d3e8173b5cdacee3110c9c039
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 23:08:21 +0100

    commit

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,12 +12,12 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
 
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     sess = Session()
-    dal = s.query(State).filter(State.name.like("%a%")).\
+    dal = sess.query(State).filter(State.name.like("%a%")).\
         order_by(State.id).all()
 
     for row in dal:
