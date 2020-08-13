285536438481ce05d75057d767fb03ceeebc05e6
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 22:58:00 +0100

    commit

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,8 @@
-<#!/usr/bin/python3
-"""a script that prints the first State object from the database hbtn_0e_6_usa """
+#!/usr/bin/python3
+"""
+a script that prints the first State object from
+the database hbtn_0e_6_usa
+"""
 
 import sys
 from model_state import Base, State
@@ -8,8 +11,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     sess = Session()
