98cd80666dd99d78def3a8253fbf2d27c82c6de4
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 18:35:48 +0100

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     dal = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = dal.cursor()
