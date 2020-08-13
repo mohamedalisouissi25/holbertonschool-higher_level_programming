539d87908bbc99c51f62367b8f2729b9e98f23fe
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 18:39:57 +0100

    commit

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cursor.execute("SELECT cities.id, cities.name, states.name\
                 FROM cities JOIN states\
                 ON cities.state_id = states.id")
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
 
     for row in rows:
         print(row)
