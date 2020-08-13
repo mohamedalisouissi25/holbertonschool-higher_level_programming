1ed33d26eb70e0e4a8a6fe81789a3d4b1d81590f
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 22:38:06 +0100

    commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-a script that lists all State objects from the database hbtn_0e_6_usa
-"""
+"""a script that lists all State objects from the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
@@ -10,12 +8,12 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                         pool_pre_ping=True)
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     sess = Session()
-    dal = s.query(State).order_by(State.id)
+    dal = sess.query(State).order_by(State.id)
 
     for row in dal:
         print("{}: {}".format(row.id, row.name))
