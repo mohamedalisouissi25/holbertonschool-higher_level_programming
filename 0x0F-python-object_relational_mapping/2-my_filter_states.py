734c728a1e106c602188e5714543d3ef281a4cc1
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 17:41:53 +0100

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     dal = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = dal.cursor()
-    que = "SELECT * FROM states\
+    query = "SELECT * FROM states\
     WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4])
     cursor.execute(query)
     rows = cursor.fetchall()
