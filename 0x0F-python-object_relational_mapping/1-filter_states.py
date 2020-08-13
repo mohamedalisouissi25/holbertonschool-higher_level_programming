ebd2a542e9d228456783d8af4dbed6594fa5edd9
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 12:19:03 +0100

    commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,12 +8,11 @@ import sys
 if __name__ == "__main__":
     dal = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cursor = dal.cursor()
-    cursor.execute("SELECT * FROM states WHERE name
-                   LIKE BINARY 'N%' ORDER BY id")
+    csr = dal.cursor()
+    csr.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cursor.fetchall()
 
     for row in rows:
         print(row)
-    cursor.close()
+    csr.close()
     dal.close()
