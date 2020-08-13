021a8a5362d6c1ac2ced226a3d6835069e01b48c
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-13 17:51:42 +0100

    commit

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a script that displays all values in the states table1 of
+hbtn_0e_0_usa where name matches the argument avoiding injection
+
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    dal = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = dal.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   (argv[4],))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+    cursor.close()
+    dal.close()
