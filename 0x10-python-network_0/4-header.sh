3f34bb205f409960717caff468b0a72a781b9e92
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-25 03:16:19 +0100

    commit

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that takes in a URL as an argument, sends a GET request to the URL
-curl -s -X -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
