821b8b582367fc436af5eca5128a2ce9b4bd1f78
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-17 21:44:40 +0100

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-const a = 'C is fun';
-const b = 'Python is cool';
-const c = 'Javascript is amazing';
-console.log('a\nb\nc');
+console.log(C is fun + '\n' + Python is cool +'\n' + Javascript is amazing);
+
