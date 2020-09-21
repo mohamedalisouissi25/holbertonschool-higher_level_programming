5bf50816e0d4370e8229d1f59c16cb56cdd851c8
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-21 22:11:58 +0100

    task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const array = [];
- for (let d = 0; d < list.length; d++) {
+  for (let d = 0; d < list.length; d++) {
     array.unshift(list[d]);
   }
   return array;
