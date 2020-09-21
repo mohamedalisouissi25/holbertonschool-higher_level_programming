699808f79eabd279667db85095a18e71c122cc36
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-21 23:42:33 +0100

    tsk101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const dct = module.require('./101-data.js').dict;
+const dct2 = {};
+for (let k in dct) {
+if (dct2[dct[k]] !== undefined) {
+    dct2[dct[k]].push(k);
+  } else {
+    dct2[dct[k]] = [k];
+  }
+}
+console.log(dct2);
