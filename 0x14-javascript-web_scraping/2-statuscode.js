c3032f750fb8e7e8858d52b5d5f5e26816942b89
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-22 13:56:01 +0100

    task2+fix1

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const req = request.get(process.argv[2]);
+request(process.argv[2], function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
