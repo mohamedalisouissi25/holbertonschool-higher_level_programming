e24e2ea95658201410d9ff284922245888cbe3da
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-22 13:41:21 +0100

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+  if (err) throw err;
+  console.log(err);
 });
