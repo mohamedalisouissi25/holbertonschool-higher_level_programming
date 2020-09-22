f6eb69472bf6076082d95239375134e1df88d2df
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-22 17:30:02 +0100

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 let comp = 0;
-const a = 0;
-const b = 0;
+let a = 0;
+let b = 0;
 request(process.argv[2], function (err, response, body) {
   if (err) { console.log(err); } else {
     const list = JSON.parse(body);
