dbbe43c95d8e4ea39afdffc65abca182271537ba
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-22 16:05:54 +0100

    task3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-let lien = 'http://swapi.co/api/films/';
-request(lien, process.argv[2], function (err, response, body) {
+const link = 'http://swapi.co/api/films/';
+request(link, process.argv[2], function (err, response, body) {
   if (err) {
     return console.log(err);
   }
