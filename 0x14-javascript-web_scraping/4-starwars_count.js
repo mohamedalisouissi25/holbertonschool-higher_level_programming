60b4911edb813d10d69812a9401cd88aab4e6545
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-22 17:07:32 +0100

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
-4 - starwars_count.js;
+#!/usr/bin/node
+const request = require('request');
+let comp = 0;
+let a = 0;
+let b = 0;
+request(process.argv[2], function (err, response, body) {
+  if (err) { console.log(err); } else {
+    const list = JSON.parse(body);
+    for (a in list.results) {
+      for (b in list.results[a].charachters) {
+        if (list.results[a].characters[b].includes('/18/')) {
+          comp++;
+        }
+      }
+    }
+    console.log(comp);
+  }
+});
