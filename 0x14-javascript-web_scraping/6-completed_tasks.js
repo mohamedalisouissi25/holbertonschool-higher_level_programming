aab88e2c6ee2409dfd58726acb4418a0a88e537d
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-22 19:58:16 +0100

    task6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const result = {};
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  const list = JSON.parse(err);
+  for (const a in result) {
+    result[list[a].userId] = 0;
+  }
+  for (const b in result) {
+    if (list[b].completed === true) {
+      if (result[list[b].userId] === undefined) {
+        result[list[b].userId] = 1;
+      } else {
+        result[list[b].userId]++;
+      }
+    }
+  }
+  console.log(result);
+});
